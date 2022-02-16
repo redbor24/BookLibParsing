ff8abafd4c54bfa91564b14a4df3830ff1f4913f
redbor <redbor24@gmail.com>
2022-02-16 14:22:16 +0300

    download books from tululu.org.

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
+import os
+from pathlib import Path
+
 import requests
-import pprint
-import json
 
-# Качаем картинку
-# url = "https://dvmn.org/filer/canonical/1542890876/16/"
-#
-# response = requests.get(url)
-# response.raise_for_status()
-#
-# filename = 'dvmn.svg'
-# with open(filename, 'wb') as file:
-#     file.write(response.content)
 
-def download_book(book_id):
+def download_book(path_to_save, book_id):
     url = f'http://tululu.org/txt.php?id={book_id}'
 
     response = requests.get(url)
     response.raise_for_status()
-    filename = f'{book_id}.txt'
+    filename = Path(path_to_save) / f'{book_id}.txt'
     with open(filename, 'w') as file:
         file.write(response.text)
 
 
 if __name__ == '__main__':
-    from_id = 32169
+    books_subfolder = 'books'
+    os.makedirs(books_subfolder, exist_ok=True)
+    from_id = 1
     for i in range(from_id, from_id + 10):
-        # print(i)
-        download_book(i)
+        download_book(books_subfolder, i)
