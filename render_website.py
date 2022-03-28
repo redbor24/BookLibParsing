f286331bb2428d27fd94dfb56fae3f0aacb229a3
redbor <redbor24@gmail.com>
2022-03-28 14:15:08 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,11 +1,17 @@
 import argparse
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+from pathlib import Path
 from urllib import parse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from livereload import Server, shell
-from more_itertools import chunked, grouper
+from livereload import Server
+from more_itertools import grouper, chunked
+from pprint import pprint
+
+
+INDEX_PAGE_CHUNK = 5  # Количество книг на одной странице
+
 
 def quote_book(book):
     return {
@@ -14,27 +20,36 @@ def quote_book(book):
         'book_path': parse.quote(book['book_path'].replace('\\', '/')),
         'title': book['title'],
         'author': book['author'],
-        'comments': book['comments'],
-        'genres': book['genres']
+        # 'comments': book['comments'],
+        # 'genres': book['genres']
     }
 
 
 def load_books(path):
     with open(path, 'r', encoding='utf-8') as f:
         books = json.load(f)
+
     return list(
-        grouper(list(map(lambda book: quote_book(book), books)), 2, None)
-                )
+        chunked(
+            grouper(list(map(lambda book: quote_book(book), books)), 2, None),
+            INDEX_PAGE_CHUNK
+        )
+    )
 
 
-def render_page(lib_path, template_path, page_path):
+def render_page(lib_path, template_path, page_path, is_debug=False):
     def render():
         template = env.get_template(template_path)
-        rendered_page = template.render(
-            books=load_books(lib_path)
-        )
-        with open(page_path, 'w', encoding="utf8") as file:
-            file.write(rendered_page)
+        books = load_books(lib_path)
+        for index, page in enumerate(books, 1):
+            rendered_page = template.render(
+                books=page
+            )
+            with open(page_path.format(index), 'w', encoding="utf8") as file:
+                file.write(rendered_page)
+
+    if is_debug:
+        render()
 
     print('Page is rerendered')
 
@@ -57,9 +72,14 @@ if __name__ == '__main__':
     )
     template_path = 'template.html'
     lib_path = 'downloaded_books.json'
-    page_path = 'index.html'
+    pages_path = 'pages'
+    os.makedirs(pages_path, exist_ok=True)
+    page_path = f'{pages_path}/index{{0}}.html'
+
+    # render_page(lib_path, template_path, page_path, True)
+    # exit()
 
     server = Server()
     server.watch(template_path, render_page(lib_path, template_path, page_path))
     server.watch('css/*.css', render_page(lib_path, template_path, page_path))
-    server.serve(root='.', port=80)
+    server.serve(root=pages_path, port=80, default_filename='index1.html')
