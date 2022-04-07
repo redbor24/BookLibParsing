9466961391981604920db8f3e597f4bbb6c6f091
redbor <redbor24@gmail.com>
2022-04-07 13:32:49 +0300

    Display books genres.

--- render_website.py
+++ render_website.py
@@ -1,3 +1,5 @@
+
+import logging
 import argparse
 import json
 import os
@@ -11,9 +13,12 @@ from pprint import pprint
 
 
 INDEX_PAGE_CHUNK = 5  # Количество книг на одной странице
+logger = logging.getLogger()
 
 
 def quote_book(book):
+    ss = '\n'.join(map(str, book['genres']))
+    print(f'ss: {ss}')
     return {
         'url': book['url'],
         'img_src': parse.quote(book['img_src'].replace('\\', '/')),
@@ -29,27 +34,29 @@ def load_books(path):
     with open(path, 'r', encoding='utf-8') as f:
         books = json.load(f)
 
-    return list(
-        chunked(
-            grouper(list(map(lambda book: quote_book(book), books)), 2, None),
-            INDEX_PAGE_CHUNK
-        )
-    )
+    lst1 = list(map(lambda book: quote_book(book), books))
+    lst2 = list(grouper(lst1, 2, None))
+    return list(chunked(lst2, INDEX_PAGE_CHUNK))
 
 
-def render_page(lib_path, template_path, page_path, is_debug=False):
+def render_page(lib_path, template_path, pages_path, page_filename, is_debug=False):
     def render():
+        logger.info('Рендерим странички...')
         template = env.get_template(template_path)
         books = load_books(lib_path)
+        logger.info(f'books: {len(books)}')
         page_count = len(books)
         for index, page in enumerate(books, 1):
+            logger.info(f'  рендерим страничку: {index}')
             rendered_page = template.render(
                 books=page,
+                pages_path=pages_path,
                 page_count=page_count,
                 page_num=index
             )
-            with open(page_path.format(index), 'w', encoding="utf8") as file:
+            with open(Path(pages_path) / page_path.format(index), 'w', encoding="utf8") as file:
                 file.write(rendered_page)
+        logger.info('Странички отрендерены')
 
     if is_debug:
         render()
@@ -69,6 +76,13 @@ if __name__ == '__main__':
     #
     # args = parser.parse_args()
 
+    logger.setLevel(logging.INFO)
+    log_handler = logging.FileHandler('LibParser.log', encoding='utf-8')
+    log_handler.setFormatter(
+        logging.Formatter('%(asctime)s - %(message)s')
+    )
+    logger.addHandler(log_handler)
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -77,15 +91,25 @@ if __name__ == '__main__':
     lib_path = 'downloaded_books.json'
     pages_path = 'pages'
     os.makedirs(pages_path, exist_ok=True)
-    page_path = f'{pages_path}/index{{0}}.html'
+    page_path = 'index{0}.html'
+
+    logger.info('------------------- Запуск программы -------------------')
+    logger.info('Параметры запуска:')
+    logger.info(f'  template_path: {template_path}')
+    logger.info(f'  lib_path: {lib_path}')
+    logger.info(f'  pages_path: {pages_path}')
+    logger.info(f'  page_path: {page_path}')
+    logger.info('--------------------------------------------------------')
+
+    render_page(lib_path, template_path, pages_path, page_path, True)
 
     server = Server()
     server.watch(
         template_path,
-        render_page(lib_path, template_path, page_path)
+        render_page(lib_path, template_path, pages_path, page_path)
     )
     server.watch(
         'css/*.css',
-        render_page(lib_path, template_path, page_path)
+        render_page(lib_path, template_path, pages_path, page_path)
     )
     server.serve(root='.', port=80, default_filename='pages/index1.html')
